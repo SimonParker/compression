cfa610426dd73ad3a259d4b606b70fd5b1927bc7
simon <sbmp2000@gmail.com>
2026-05-05 14:19:27 -0700

    first thoughts

---
+++ simzip.c
@@ -0,0 +1,18 @@
+//simon parker
+
+/*
+
+general code idea:
+  - tree data structure and corresponding functionality
+  - building of the huffman tree
+    - scanning the file
+    -analyzing file
+    - creating codes (look into how to do prefix free encoding)
+    -creating tree
+  - encoding the file based on the tree
+    - writing code binary to file
+
+
+  -decoder that turns the compressed file into .txt again
+
+*/
